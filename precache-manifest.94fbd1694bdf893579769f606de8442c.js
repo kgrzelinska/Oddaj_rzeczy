915d6d9b46c3358a92965c6a96b32a37107dd7b9
Klaudia Grzelińska <kl.grzelinska@gmail.com>
2020-07-21 21:31:02 +0200

    Updates

---
+++ precache-manifest.94fbd1694bdf893579769f606de8442c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148facd1514753fa9cf75594648a6dbb",
+    "url": "/oddaj_rzeczy/index.html"
+  },
+  {
+    "revision": "9c1febbda1263f571bd5",
+    "url": "/oddaj_rzeczy/static/css/main.b33ac340.chunk.css"
+  },
+  {
+    "revision": "88819ceb8678ccf2d186",
+    "url": "/oddaj_rzeczy/static/js/2.7a6becb5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/oddaj_rzeczy/static/js/2.7a6becb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1febbda1263f571bd5",
+    "url": "/oddaj_rzeczy/static/js/main.001af33c.chunk.js"
+  },
+  {
+    "revision": "53d0cfdca76e90fe8cb7",
+    "url": "/oddaj_rzeczy/static/js/runtime-main.71818998.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/oddaj_rzeczy/static/media/3 Columns Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddaj_rzeczy/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddaj_rzeczy/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/oddaj_rzeczy/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddaj_rzeczy/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddaj_rzeczy/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddaj_rzeczy/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddaj_rzeczy/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/oddaj_rzeczy/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddaj_rzeczy/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddaj_rzeczy/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "ef2519d20c70fe42a96e8c744772c6ef",
+    "url": "/oddaj_rzeczy/static/media/header.ef2519d2.png"
+  }
+]);
